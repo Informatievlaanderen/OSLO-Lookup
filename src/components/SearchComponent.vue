962b589e363a748d3092f47ba1fc8db31c7bc1ed
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-05-08 17:05:26 +0200

    Created search component and inilizated web components

---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "SearchComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+import {
+  VlCore,
+  VlUtil
+} from'@govflanders/vl-ui-vue-components';
+
+Vue.use(VlCore);
+Vue.use(VlUtil);
 
 new Vue({
   render: h => h(App),
